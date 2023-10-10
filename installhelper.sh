53f6577c18d55a59d7c28dd0e731d6da04e53eb5
hootisman <david1zalewski@gmail.com>
2023-10-10 19:10:48 -0400

    edited

--- installhelper.sh
+++ installhelper.sh
@@ -5,5 +5,7 @@ path=$(pwd)
 cp -r "$path/output"/* "$1/kubejs/server_scripts/pmmo/"
 echo "kubejs scripts installed in path : " + $1
 
-cp -r "$path/datapack"/* "$2/data/minecraft/pmmo/items/"
+cp -r "$path/datapack/armor" "$2/data/minecraft/pmmo/items/"
+cp -r "$path/datapack/item" "$2/data/minecraft/pmmo/items/"
+cp -r "$path/datapack/block" "$2/data/minecraft/pmmo/blocks/"
 echo "datapack scripts installed in path : " + $2
